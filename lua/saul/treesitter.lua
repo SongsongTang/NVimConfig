92e884b430217d80f45d50d8b24f9f4d542d5b4d
SongsongTang <tss1375716572@163.com>
2022-11-07 20:00:27 +0800

    reduce first start time

--- lua/saul/treesitter.lua
+++ lua/saul/treesitter.lua
@@ -1,10 +1,10 @@
-require 'nvim-treesitter.install'.compilers = {'zig'}
+--require 'nvim-treesitter.install'.compilers = {'zig'}
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
 	return
 end
 configs.setup({
-	ensure_installed = "all", -- one of "all" or a list of languages
+	ensure_installed = {"lua", "c", "cpp", "python", "latex", "verilog", "cmake"}, -- one of "all" or a list of languages
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	highlight = {
 		enable = true, -- false will disable the whole extension
